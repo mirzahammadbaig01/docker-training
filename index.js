2e2f99e1454b066e522b4842c63f8fc9b52061b7
mirzahammadbaig01 <hammad.baig@devsinc.com>
2023-01-23 22:11:01 +0500

    configured host

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const { response } = require("express");
 const express = require("express");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
+
+app.get("/", async (req, res) => {
+  const db = new MongoClient("mongodb://admin:admin@localhost:27018");
+  if (await db.connect()) {
+    console.log("connected");
+    console.log(db);
+  }
+  res.send("hello");
+});
 app.listen(3000, () => {
   console.log("App is listing on this post 3000");
-});
-app.get("/", (req, res) => {
-  MongoClient.connect("mongodb://admin:admin@localhost:27017"),
-    function (err, client) {
-      if (err) throw err;
-      console.log(client);
-      res.send("hello");
-    };
 });
