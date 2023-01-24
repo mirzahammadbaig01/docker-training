a30ffad6b35a09a28c02d5b2c45e318dfa2709be
mirzahammadbaig01 <hammad.baig@devsinc.com>
2023-01-24 18:39:03 +0500

    Add dockerfile

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const MongoClient = require("mongodb").MongoClient;
 
 app.get("/", async (req, res) => {
-  const db = new MongoClient("mongodb://admin:admin@0.0.0.0:27018");
+  const db = new MongoClient("mongodb://admin:admin@localhost:27018");
   if (await db.connect()) {
     console.log("connected");
   }
